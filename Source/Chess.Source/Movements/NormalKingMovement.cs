d245cf7a1c3aaf83059b1ac126e5546d9fbdf9dd
totkov <neznamue@gmail.com>
2015-09-10 17:18:20 +0300

    Implement king movement logic in NormalKingMovement.cs

--- Source/Chess.Source/Movements/NormalKingMovement.cs
+++ Source/Chess.Source/Movements/NormalKingMovement.cs
@@ -1,5 +1,7 @@
 ﻿namespace Chess.Source.Movements
 {
+    using System;
+
     using Chess.Source.Board.Contracts;
     using Chess.Source.Common;
     using Chess.Source.Figures.Contracts;
@@ -9,7 +11,28 @@
     {
         public void VlidateMove(IFigure figure, IBoard board, Move move)
         {
-            throw new System.NotImplementedException();
+            Position from = move.From;
+            Position to = move.To;
+
+            bool condition = (
+                ((from.Col + 1) == to.Col) ||
+                ((from.Col - 1) == to.Col) ||
+                ((from.Row + 1) == to.Row) ||
+                ((from.Row - 1) == to.Row) ||
+                (((from.Row + 1) == to.Row) && ((from.Col + 1) == to.Col)) ||
+                (((from.Row - 1) == to.Row) && ((from.Col + 1) == to.Col)) ||
+                (((from.Row - 1) == to.Row) && ((from.Col - 1) == to.Col)) ||
+                (((from.Row + 1) == to.Row) && ((from.Col - 1) == to.Col))
+                );
+
+            if (condition)
+            {
+                return;
+            }
+            else
+            {
+                throw new InvalidOperationException("King cannot move this way!");
+            }
         }
     }
 }
